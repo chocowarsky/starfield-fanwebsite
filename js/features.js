05f39692d090afa1fac747bff71c0e366a93a993
Jilkatras <bagus27wijaya@gmail.com>
2025-10-14 15:16:20 +0800

    Features-2

---
+++ js/features.js
@@ -0,0 +1,9 @@
+// const features = ["ALPHA CENTAURI", "CONSTELLATION", "GRAV DRIVE", "ASTERIUM", "NOVA CORP"];
+// const track = document.querySelector(".scroll-track");
+
+// // isi 2x biar looping
+// for (let i = 0; i < 2; i++) {
+//   features.forEach(f => {
+//     track.innerHTML += `<div class="feature-card">${f}</div>`;
+//   });
+// }
